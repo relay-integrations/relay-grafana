699e272bca8b10b582c18e205e785b62b2586ca1
Rick Lane <rick.lane@puppet.com>
2021-06-30 21:08:20 -0700

    Add Grafana get dashboard step

---
+++ steps/get-dashboard/README.md
@@ -0,0 +1,3 @@
+# grafana-step-get-dashboard
+
+This step retrieves the data for a [Grafana dashboard](https://grafana.com/docs/grafana/latest/dashboards/) using the [Grafana Dashboard API](https://grafana.com/docs/grafana/latest/http_api/dashboard/).
---
+++ steps/get-dashboard/step.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import requests
+import os
+import time
+from urllib.parse import urljoin
+from relay_sdk import Interface, Dynamic as D
+
+relay = Interface()
+
+apiURL = relay.get(D.connection.apiURL)
+apiKey = relay.get(D.connection.apiKey)
+
+uid = relay.get(D.uid)
+
+r = requests.get(
+    urljoin(apiURL, f'/api/dashboards/uid/{uid}'),
+    headers={'Authorization': 'Bearer ' + apiKey},
+)
+
+print('Emitted event to Grafana API, got response: ', r.text)
+
+r.raise_for_status()
